a79f0f5c2cd59a331f6928834dfc76c2ba0e7b2d
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-11-24 21:14:36 -0200

    tabReducers/ tabActions

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <div>
-    <h1>Ciclo de pagamento</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+
+
+class BillingCycle extends Component {
+  render(){
+    return(
+      <div>
+        <ContentHeader title='Ciclos de pagamentos' small='Cadastro'/>
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='listar' icon='bars' target='tabList' />
+              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+            </TabsContent>
+          </Tabs> 
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default BillingCycle
--- frontend/src/main/reducers.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-  dashboard: () => ({summary: {credit: 100, debt: 50}})
-})
-
-export default rootReducer
