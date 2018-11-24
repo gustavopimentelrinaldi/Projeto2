84a895afb63ebb33a994ac87b116932c82f48826
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-11-24 15:43:14 -0200

    grid/widget

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default props => (
   <footer className='main-footer'>
     <strong>
-      Copyright $copy; 2018
-      <a href='http://github.com/gustavopimentelrinaldi'>Rinaldi</a>
+      Copyright &copy; 2018
+      <a href='http://github.com/gustavopimentelrinaldi'> Rinaldi</a>
     </strong>
   </footer>
 )
