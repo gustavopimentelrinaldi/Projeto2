22c9857b357ad9aec1810f81b8f1812d97a1e0cc
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-11-25 21:34:04 -0200

    summary/sumofDebts and credits

--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux'
 import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operador/if'
 
 class ItemList extends Component {
 
@@ -29,6 +30,11 @@ class ItemList extends Component {
         <td><Field name={`${this.props.field}[${index}].value`} component={Input}
           placeholder='Informe o valor' readOnly={this.props.readOnly}
         /></td>
+        <If test={this.props.showStatus}>
+          <td><Field name={`${this.props.field}[${index}].status`} component={Input}
+            placeholder='Informe o status' readOnly={this.props.readOnly}
+          /></td>
+        </If>
         <td>
           <button type='button' className='btn btn-success'
           onClick={() => this.add(index+ 1)}>
@@ -56,6 +62,9 @@ class ItemList extends Component {
             <thead>
               <th>Nome</th>
               <th>Valor</th>
+              <If test={this.props.showStatus}>
+                <th>Status</th>
+              </If>
               <th className='table-actions'>Ações</th>
             </thead>
             <tbody>
